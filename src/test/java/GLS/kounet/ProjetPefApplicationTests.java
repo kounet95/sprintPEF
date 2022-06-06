fef37c6e2e0748e921af3c8d6b966c33b8bd35b6
kounet95 <85202452+kounet95@users.noreply.github.com>
2022-06-06 05:44:46 +0100

    pef

---
+++ src/test/java/GLS/kounet/ProjetPefApplicationTests.java
@@ -0,0 +1,13 @@
+package GLS.kounet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
